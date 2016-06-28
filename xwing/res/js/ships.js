40b182cc803cd0970f1759a288394488a95e0d3f
Danurai <Danurai@users.noreply.github.com>
2016-06-28 18:10:24 +0100

    Add files via upload

--- xwing/res/js/ships.js
+++ xwing/res/js/ships.js
@@ -1,7 +1,11 @@
 [
+
+
+
 	{
-		"faction": "Imperial",
+		"faction": "imperial",
 		"name": "Tie Fighter",
+		"pilot": "Academy Pilot",
 		"actions": ["f", "e", "r"],
 		"size": "small",
 		"symbol": "F",
@@ -35,9 +39,16 @@
 			}
 		}
 	},
+
+
+
+
+
+
 	{
-		"faction": "Imperial",
+		"faction": "imperial",
 		"name": "Tie Advanced",
+		"pilot": "Tempest Squadron pilot",
 		"actions": ["f", "l", "r", "e"],
 		"size": "small",
 		"symbol": "A",
@@ -70,9 +81,16 @@
 			}
 		}
 	},
+
+
+
+
+
+
 	{
-		"faction": "Imperial",
+		"faction": "imperial",
 		"name": "Tie Interceptor",
+		"pilot": "Alpha Squadron Pilot",
 		"actions": ["f", "r", "b", "e"],
 		"size": "small",
 		"symbol": "I",
@@ -106,11 +124,140 @@
 			}
 		}
 	},
-	
+
+
+
+
+
 
 	{
-		"faction": "Imperial",
+		"faction": "imperial",
+		"name": "Tie Defender",
+		"pilot": "Delta Squadron Pilot",
+		"actions": ["f", "l", "r"],
+		"size": "small",
+		"symbol": "D",
+		"points": "30",
+		"ps": "1",
+		"attack": 3,
+		"evade": 3,
+		"hull": 3,
+		"shield": 3,
+		"moveset": {
+			"5": {
+				"S": "g"
+			},
+			"4": {
+				"S": "g",
+				"K": "w"
+			},
+			"3": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "r"
+			},
+			"1": {
+				"B": "w",
+				"T": "r"
+			}
+		}
+	},
+
+
+
+
+
+
+	{
+		"faction": "imperial",
+		"name": "Tie Phantom",
+		"pilot": "Sigma Squadron Pilot",
+		"actions": ["f", "r", "e", "k"],
+		"size": "small",
+		"symbol": "P",
+		"points": "25",
+		"ps": "3",
+		"attack": 4,
+		"evade": 2,
+		"hull": 2,
+		"shield": 2,
+		"moveset": {
+			"4": {
+				"S": "w",
+				"K": "r"
+			},
+			"3": {
+				"S": "g",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"T": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "imperial",
+		"name": "Tie Bomber",
+		"pilot": "Scimitar Squadron Pilot",
+		"actions": ["f", "l", "r"],
+		"size": "small",
+		"symbol": "B",
+		"points": "16",
+		"ps": "2",
+		"attack": 2,
+		"evade": 2,
+		"hull": 6,
+		"shield": 0,
+		"moveset": {
+			"5": {
+				"K": "r"
+			},
+			"4": {
+				"S": "w"
+			},
+			"3": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "r"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+
+	{
+		"faction": "imperial",
 		"name": "Firespray-31",
+		"pilot": "Bounty Hunter",
 		"actions": ["f", "l", "e"],
 		"size": "large",
 		"symbol": "f",
@@ -148,8 +295,9 @@
 
 
 	{
-		"faction": "Imperial",
+		"faction": "imperial",
 		"name": "Lambda-Class Shuttle",
+		"pilot": "Omicron Group Pilot",
 		"actions": ["f", "l"],
 		"size": "large",
 		"symbol": "l",
@@ -184,8 +332,9 @@
 
 
 	{
-		"faction": "Imperial",
+		"faction": "imperial",
 		"name": "VT-49 DECIMATOR",
+		"pilot": "Patrol Leader",
 		"actions": ["f", "l"],
 		"size": "large",
 		"symbol": "d",
@@ -212,6 +361,602 @@
 			"1": {
 				"S": "w",
 				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "X-Wing",
+		"pilot": "Rookie Pilot",
+		"actions": ["f", "l"],
+		"size": "small",
+		"symbol": "x",
+		"points": "21",
+		"ps": "2",
+		"attack": 3,
+		"evade": 2,
+		"hull": 3,
+		"shield": 2,
+		"moveset": {
+			"4": {
+				"S": "w",
+				"K": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "w"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "E-Wing",
+		"pilot": "Knave Squadron Pilot",
+		"actions": ["f", "l", "r", "e"],
+		"size": "small",
+		"symbol": "e",
+		"points": "27",
+		"ps": "1",
+		"attack": 3,
+		"evade": 3,
+		"hull": 2,
+		"shield": 3,
+		"moveset": {
+			"5": {
+				"S": "w"
+			},
+			"4": {
+				"S": "w",
+				"K": "r"
+			},
+			"3": {
+				"S": "g",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "A-Wing",
+		"pilot": "Prototype Pilot",
+		"actions": ["f", "l", "r"],
+		"size": "small",
+		"symbol": "a",
+		"points": "17",
+		"ps": "1",
+		"attack": 2,
+		"evade": 3,
+		"hull": 2,
+		"shield": 2,
+		"moveset": {
+			"5": {
+				"S": "g",
+				"K": "r"
+			},
+			"4": {
+				"S": "g"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "Z-95 Headhunter",
+		"pilot": "Bandit Squadron Pilot",
+		"actions": ["f", "l", "b", "e"],
+		"size": "small",
+		"symbol": "z",
+		"points": "12",
+		"ps": "1",
+		"attack": 2,
+		"evade": 3,
+		"hull": 2,
+		"shield": 2,
+		"moveset": {
+			"4": {
+				"S": "w"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "B-Wing",
+		"pilot": "Blue Squadron Pilot",
+		"actions": ["f", "l", "r"],
+		"size": "small",
+		"symbol": "b",
+		"points": "22",
+		"ps": "2",
+		"attack": 3,
+		"evade": 1,
+		"hull": 3,
+		"shield": 5,
+		"moveset": {
+			"4": {
+				"S": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"1": {
+				"S": "g",
+				"B": "g",
+				"T": "r"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "Y-Wing",
+		"pilot": "Gold Squadron Pilot",
+		"actions": ["f", "l"],
+		"size": "small",
+		"symbol": "y",
+		"points": "18",
+		"ps": "2",
+		"attack": 2,
+		"evade": 1,
+		"hull": 5,
+		"shield": 3,
+		"moveset": {
+			"4": {
+				"S": "r",
+				"K": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "HWK-290",
+		"pilot": "Rebel Operative",
+		"actions": ["f", "l"],
+		"size": "small",
+		"symbol": "h",
+		"points": "16",
+		"ps": "2",
+		"attack": 1,
+		"evade": 2,
+		"hull": 4,
+		"shield": 1,
+		"moveset": {
+			"4": {
+				"S": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "rebel",
+		"name": "YT-1300",
+		"pilot": "Outer Rim Smuggler",
+		"actions": ["f", "l"],
+		"size": "large",
+		"symbol": "m",
+		"points": "27",
+		"ps": "1",
+		"attack": 2,
+		"evade": 1,
+		"hull": 6,
+		"shield": 4,
+		"moveset": {
+			"4": {
+				"S": "w",
+				"K": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "M3A Interceptor",
+		"pilot": "Cartel Spacer",
+		"actions": ["f", "l", "r", "e"],
+		"size": "small",
+		"symbol": "s",
+		"points": "14",
+		"ps": "2",
+		"attack": 2,
+		"evade": 3,
+		"hull": 2,
+		"shield": 1,
+		"moveset": {
+			"5": {
+				"K": "r"
+			},
+			"4": {
+				"S": "w"
+			},
+			"3": {
+				"S": "g",
+				"B": "w"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"B": "g",
+				"T": "w"
+			}
+		}
+	},
+
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "StarViper",
+		"pilot": "Black Sun Enforcer",
+		"actions": ["f", "l", "r", "e"],
+		"size": "small",
+		"symbol": "v",
+		"points": "25",
+		"ps": "1",
+		"attack": 3,
+		"evade": 3,
+		"hull": 4,
+		"shield": 1,
+		"moveset": {
+			"4": {
+				"S": "w"
+			},
+			"3": {
+				"S": "g",
+				"B": "w",
+				"L": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "Aggressor",
+		"pilot": "IG88A",
+		"actions": ["f", "l", "b", "e"],
+		"size": "large",
+		"symbol": "i",
+		"points": "36",
+		"ps": "6",
+		"attack": 3,
+		"evade": 3,
+		"hull": 4,
+		"shield": 4,
+		"moveset": {
+			"4": {
+				"K": "r"
+			},
+			"3": {
+				"S": "g",
+				"B": "g",
+				"L": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "Firespray-31",
+		"pilot": "Mandalorian Mercenary",
+		"actions": ["f", "l", "e"],
+		"size": "large",
+		"symbol": "f",
+		"points": "35",
+		"ps": "5",
+		"attack": 3,
+		"evade": 2,
+		"hull": 6,
+		"shield": 4,
+		"moveset": {
+			"4": {
+				"S": "w",
+				"K": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "Z-95 headhunter",
+		"pilot": "Binayre Pirate",
+		"actions": ["f", "l"],
+		"size": "small",
+		"symbol": "z",
+		"points": "12",
+		"ps": "1",
+		"attack": 2,
+		"evade": 2,
+		"hull": 2,
+		"shield": 2,
+		"moveset": {
+			"4": {
+				"S": "w"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "w",
+				"K": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "g",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "Y-Wing",
+		"pilot": "Syndicate Thug",
+		"actions": ["f", "l"],
+		"size": "small",
+		"symbol": "y",
+		"points": "18",
+		"ps": "2",
+		"attack": 2,
+		"evade": 1,
+		"hull": 5,
+		"shield": 3,
+		"moveset": {
+			"4": {
+				"S": "r",
+				"K": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "w",
+				"T": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "w"
+			}
+		}
+	},
+
+
+
+
+
+	{
+		"faction": "scum",
+		"name": "HWK-290",
+		"pilot": "Spice Runner",
+		"actions": ["f", "l"],
+		"size": "small",
+		"symbol": "h",
+		"points": "16",
+		"ps": "1",
+		"attack": 1,
+		"evade": 2,
+		"hull": 4,
+		"shield": 1,
+		"moveset": {
+			"4": {
+				"S": "r"
+			},
+			"3": {
+				"S": "w",
+				"B": "r"
+			},
+			"2": {
+				"S": "g",
+				"B": "w",
+				"T": "w"
+			},
+			"1": {
+				"S": "g",
+				"B": "g"
 			}
 		}
 	}
